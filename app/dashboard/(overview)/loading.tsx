bbe8a8bb0e74d8e407c8402962959e29ff108c10
Daniel_MQ <danielmq117@gmail.com>
2024-09-05 16:34:21 -0600

    Add: Esqueleto a la pagina de 'dashboard'.

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import { lusitana } from "@/app/ui/fonts";
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+    // return <div className={`${lusitana.className} text-xl`}>Loading...</div>;
+
+    return <DashboardSkeleton />;
+}
--- app/dashboard/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { lusitana } from "@/app/ui/fonts";
-
-export default function Loading() {
-    return <div className={`${lusitana.className} text-xl`}>Loading...</div>;
-}

aa0e6cf900e2e9dbff5184a255567b0b59a018c7
Daniel_MQ <danielmq117@gmail.com>
2024-09-05 18:46:10 -0600

    Feat: Partial Pre-rendering.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: "incremental", // Habilitar la pre-renderizacion parcial
+    },
+};
 
 export default nextConfig;
